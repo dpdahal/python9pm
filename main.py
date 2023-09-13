446cf1e0e33eec4c5bdc45ab1538eca5b34a339e
dpdahal <dp48dahal@gmail.com>
2023-09-13 21:39:35 +0545

    update

---
+++ main.py
@@ -0,0 +1,98 @@
+# print("Hello python")
+# print("hello",345,'python')
+# print "hello"
+
+# print('mother\'s')
+
+# name = 'ram'
+# age = 20
+# phone = 45678
+# print("my name is " + name + " age "+str(age))
+# print("My name is {} age {} phone {}".format(name,age,phone))
+# print(f"My name is {name} age {age} phone {phone}")
+
+# name = 56.888
+# print(type(name))
+
+# number: int,float,complex
+# string 
+# boolean: True or False
+# None
+# list
+# tuple
+# set 
+# dictionary
+
+# 1=5
+#$%
+# _=50
+
+# myusername='ram'
+
+# MyUserName: pascle case: class
+# my_user_name: snake case: function
+# myUserName: camel case: variable
+
+# python is object oriented language
+
+# x=10
+
+# num =347834567865
+# n1 =45.578
+# n2 = 5+6j
+
+# a =True
+
+# def test():
+#     pass
+
+# print(test())
+
+# name ='sophia'
+# print(dir(name))
+# print(name)
+
+# course ='we are learning python'
+# print(course.upper())
+# print(course.title())
+
+# a =5
+# b=7
+
+# *a,b = 5,'ram',67,89,8,7,89,98
+# print(a)
+# print(b)
+
+# x =int(input("Enter x: "))
+# y = int(input("Enter y: "))
+# print(x+y)
+
+# a =(5+6)*7/5
+# print(a)
+
+# list 
+
+# name ='sophia'
+# print(name[0])
+
+# course ='we are learning python'
+# print(course[3:7])
+
+# data=['ram','sita']
+# data.append('hari')
+# data.insert(0,'gita')
+# print(data)
+
+# append
+# copy
+# count
+# index
+# insert
+# pop
+# remove
+
+# print(data[:2])
+# print(dir(data))
+# print(data[0])
+
+
